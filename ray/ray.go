a8b65679cf6434c3d4bee5708ddd9ad35eb37844
jpatidar30 <jpatidar@deqode.com>
2024-09-18 19:56:22 +0530

    fix: logs

--- ray/ray.go
+++ ray/ray.go
@@ -171,7 +171,7 @@ func (c rayRestClient) RunTask(ctx context.Context, cfg TaskConfig) (string, err
 	
 	entrypoint := fmt.Sprintf(`python3 -c """%s"""`, scriptContent)
 
-	_, err := submitJob(ctx, cfg.Task.RayClusterEndpoint, entrypoint, "127")
+	_, err = submitJob(ctx, cfg.Task.RayClusterEndpoint, entrypoint, "127")
 	if err != nil {
 		return "", err
 	}
@@ -192,7 +192,7 @@ func (c rayRestClient) RunServeTask(ctx context.Context, cfg TaskConfig) (string
 		return "", fmt.Errorf("failed to generate ray serve script: %w", err)
 	}
 	rayServeEntrypoint := fmt.Sprintf(`python3 -c """%s"""`, rayServeScript)
-	response, err := submitJob(ctx, cfg.Task.RayClusterEndpoint, rayServeEntrypoint, "128")
+	_, err := submitJob(ctx, cfg.Task.RayClusterEndpoint, rayServeEntrypoint, "128")
 	if err != nil {
 		return "", fmt.Errorf("failed to submit ray serve job: %w", err)
 	}
