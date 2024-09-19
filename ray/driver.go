e4a2b07711e2016b7c9ceb69e851e2a3f6bb61c0
jpatidar30 <jpatidar@deqode.com>
2024-09-19 18:39:07 +0530

    feat: delete actor on stoping task

--- ray/driver.go
+++ ray/driver.go
@@ -432,12 +432,16 @@ func (d *Driver) StopTask(taskID string, timeout time.Duration, signal string) e
 		return fmt.Errorf("timed out waiting for remote task (id=%s) to stop (detach=%t)",
 			taskID, detach)
 	}
-	_, rayServeHealthErr := d.client.DeleteActor(context.Background(), GlobalConfig.TaskConfig)
-
 	f, err := fifo.OpenWriter(handle.taskConfig.StdoutPath)
 
-	if rayServeHealthErr != nil {
-		fmt.Fprintf(f, "Failed to stop remote task [%s] - [%s] \n", GlobalConfig.TaskConfig.Task.Actor, runServeTaskErr)
+	if err != nil {
+		fmt.Fprintf(f, "Failed to open writer while stopping \n")
+	}
+	_, err = d.client.DeleteActor(context.Background(), GlobalConfig.TaskConfig)
+
+
+	if err != nil {
+		fmt.Fprintf(f, "Failed to stop remote task [%s] - [%s] \n", GlobalConfig.TaskConfig.Task.Actor, err)
 	}
 	fmt.Fprintf(f, "remote task stopped - [%s]\n", taskID)
 	d.logger.Info("remote task stopped", "task_id", taskID, "timeout", timeout, "signal", signal)
