73f90cc34039fe0984cd9b0549ddb756eea3e6e8
jpatidar30 <jpatidar@deqode.com>
2024-12-30 15:32:32 +0530

    chore: update ray api server port

--- templates/ray_serve_api.go
+++ templates/ray_serve_api.go
@@ -2,96 +2,101 @@ package templates
 
 const RayServeAPITemplate = `
 from fastapi import FastAPI, Request, HTTPException, Query
-import asyncio
-import uvicorn
+from fastapi.responses import JSONResponse
+from ray import serve
+import subprocess
 import ray
 
+serve.shutdown()
 app = FastAPI()
+serve.start(detached=True, http_options={\"host\": \"0.0.0.0\", \"port\": 8001})
 
-@app.get(\"/api/health\")
-async def health_check():
-    return {\"status\": \"ok\"}
+@serve.deployment(route_prefix=\"/api\")
+@serve.ingress(app)
+class {{.ServerName}}:
+    def __init__(self):
+        pass
 
-@app.post(\"/api/actor-status\")
-async def actor_status(request: Request):
-    if request.headers.get(\"content-type\") != \"application/json\":
-        raise HTTPException(status_code=400, detail=\"Request must be JSON\")
+    @app.get(\"/health\")
+    async def actor_status(self, request: Request):
+        return {\"status\": \"ok\"}
 
-    data = await request.json()
-    actor_id = data.get(\"actor_id\")
+       
+    @app.post(\"/actor-status\")
+    async def actor_status(self, request: Request):
+        if request.headers.get(\"content-type\") != \"application/json\":
+            raise HTTPException(status_code=400, detail=\"Request must be JSON\")
 
-    if not actor_id:
-        raise HTTPException(status_code=400, detail=\"No actor_id provided\")
+        data = await request.json()
+        actor_id = data.get(\"actor_id\")
+
+        if not actor_id:
+            raise HTTPException(status_code=400, detail=\"No actor_id provided\")
     
-    try:
-        command = f\"ray list actors --filter 'state=ALIVE' | grep {actor_id}\"
-        process = await asyncio.create_subprocess_shell(
-            command, stdout=asyncio.subprocess.PIPE, stderr=asyncio.subprocess.PIPE
-        )
-        stdout, stderr = await process.communicate()
-
-        if process.returncode == 0:
-            processed_output = ' '.join(stdout.decode().strip().split())
-            actor_status = processed_output.split(' ')[3] if len(processed_output.split(' ')) >= 4 else \"Unavailable\"
-            return {\"status\": \"success\", \"actor_status\": actor_status}
-        else:
-            return {\"status\": \"error\", \"error\": stderr.decode().strip()}
-
-    except Exception as e:
-        raise HTTPException(status_code=500, detail=\"Failed to get actor status\")
-
-@app.post(\"/api/actor-logs\")
-async def actor_logs(request: Request):
-    if request.headers.get(\"content-type\") != \"application/json\":
-        raise HTTPException(status_code=400, detail=\"Request must be JSON\")
-
-    data = await request.json()
-    actor_id = data.get(\"actor_id\")
-
-    if not actor_id:
-        raise HTTPException(status_code=400, detail=\"No actor_id provided\")
-
-    try:
-        command = f\"ray list actors --filter 'state=ALIVE' | grep {actor_id}\"
-        process = await asyncio.create_subprocess_shell(
-            command, stdout=asyncio.subprocess.PIPE, stderr=asyncio.subprocess.PIPE
-        )
-        stdout, stderr = await process.communicate()
-
-        if process.returncode == 0:
-            processed_output = ' '.join(stdout.decode().strip().split())
-            id = processed_output.split(' ')[1] if len(processed_output.split(' ')) >= 4 else \"Unavailable\"
-
-            command_logs = f\"ray logs actor --id {id} --tail 100\"
-            process_logs = await asyncio.create_subprocess_shell(
-                command_logs, stdout=asyncio.subprocess.PIPE, stderr=asyncio.subprocess.PIPE
+        try:
+            command = f\"ray list actors --filter 'state=ALIVE' | grep {actor_id}\"
+            result = subprocess.run(
+                command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True
             )
-            stdout_logs, stderr_logs = await process_logs.communicate()
 
-            if process_logs.returncode == 0:
-                return {\"status\": \"success\", \"logs\": stdout_logs.decode().strip()}
+            if result.returncode == 0:
+                processed_output = ' '.join(result.stdout.strip().split())
+                actor_status = processed_output.split(' ')[3] if len(processed_output.split(' ')) >= 4 else \"Unavailable\"
+                print(actor_status)
+                return {\"status\": \"success\", \"actor_status\": actor_status}
             else:
-                return {\"status\": \"error\", \"error\": stderr_logs.decode().strip()}
+                return {\"status\": \"error\", \"error\": result.stderr.strip()}
 
-        else:
-            return {\"status\": \"error\", \"error\": stderr.decode().strip()}
+        except Exception as e:
+            raise HTTPException(status_code=500, detail=\"Failed to get actor\")
 
-    except Exception as e:
-        raise HTTPException(status_code=500, detail=\"Failed to get actor logs\")
+    @app.post(\"/actor-logs\")
+    async def actor_logs(self, request: Request):
+        if request.headers.get(\"content-type\") != \"application/json\":
+            raise HTTPException(status_code=400, detail=\"Request must be JSON\")
 
-@app.delete(\"/api/kill-actor\")
-async def kill_actor(actor_id: str = Query(...)):
-    if not actor_id:
-        raise HTTPException(status_code=400, detail=\"No actor_id provided\")
+        data = await request.json()
+        actor_id = data.get(\"actor_id\")
 
-    try:
-        actor = ray.get_actor(name=f\"{actor_id}\", namespace=\"{{.Namespace}}\")
-        ray.kill(actor)
-        return {\"status\": \"success\", \"detail\": f\"{actor_id} deleted\"}
+        if not actor_id:
+            raise HTTPException(status_code=400, detail=\"No actor_id provided\")
 
-    except Exception as e:
-        raise HTTPException(status_code=500, detail=f\"Failed to kill actor: {str(e)}\")
+        try:
+            command = f\"ray list actors --filter 'state=ALIVE' | grep {actor_id}\"
+            result = subprocess.run(
+                command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True
+            )
 
-if __name__ == \"__main__\":
-    uvicorn.run(app, host=\"0.0.0.0\", port=8000)
+            if result.returncode == 0:
+                processed_output = ' '.join(result.stdout.strip().split())
+                id = processed_output.split(' ')[1] if len(processed_output.split(' ')) >= 4 else \"Unavailable\"
+
+            command = f\"ray logs actor --id {id} --tail 100\"
+            result = subprocess.run(
+                command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True
+            )
+
+            if result.returncode == 0:
+                return {\"status\": \"success\", \"logs\": result.stdout.strip()}
+            else:
+                return {\"status\": \"error\", \"error\": result.stderr.strip()}
+
+        except Exception as e:
+            raise HTTPException(status_code=500, detail=\"Failed to get actor logs\")
+            
+    @app.delete(\"/kill-actor\")
+    async def kill_actor(self, actor_id: str = Query(...)):
+        if not actor_id:
+            raise HTTPException(status_code=400, detail=\"No actor_id provided\")
+
+        try:
+            actor = ray.get_actor(name=f\"{actor_id}\", namespace=\"{{.Namespace}}\")
+            ray.kill(actor)
+            return {\"status\": \"success\", \"detail\": f\"{actor_id} deleted\"}
+
+        except Exception as e:
+            raise HTTPException(status_code=500, detail=f\"Failed to kill actor: {str(e)}\")
+
+# Deploy the model
+serve.run({{.ServerName}}.bind())
 `
