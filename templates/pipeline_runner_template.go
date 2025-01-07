d071892a3527f9d64a7e52251a5a8152ae46cc34
Ajaiswal2610 <ajaiswal@deqode.com>
2025-01-07 18:27:34 +0530

    add async calls and signal handles

--- templates/pipeline_runner_template.go
+++ templates/pipeline_runner_template.go
@@ -40,34 +40,54 @@ import ray
 import os
 import sys
 import importlib
-import signal
-import time
 
-ray.init(address=\"auto\", namespace=\"{{.Namespace}}\")
-@ray.remote(num_cpus={{.NumCPUs}})
+ray.init(address=\"auto\", namespace=\"{{.Namespace}}\" , num_cpus=\"{{.NumCPUs}}\")
+task_ref = None
+
+# Signal handler function to cancel the Ray task
+def signal_handler(signum, frame):
+    global task_ref
+    print(f\"Signal {signum} received. Attempting to cancel the task...\")
+    if task_ref:
+        ray.cancel(task_ref, force=True)  # Cancel the remote task
+        print(\"Task canceled.\")
+    sys.exit(0)  # Exit the program gracefully
 
 
+async def f():
+    directory_path = os.path.dirname(\"{{.PipelineFilePath}}\")
+    file_name = os.path.splitext(os.path.basename(\"{{.PipelineFilePath}}\"))[0]
 
-def main():
+    sys.path.append(directory_path)
+
+    # Dynamically import the pipeline module
+    pipeline_module = importlib.import_module(file_name)
+
+    # Execute the pipeline function directly
+    getattr(pipeline_module, \"{{.PipelineRunner}}\")()
+
+
+@ray.remote
+def wrapper():
+    loop = asyncio.new_event_loop()  # Create a new event loop for the worker
+    asyncio.set_event_loop(loop)
+    loop.run_until_complete(f())
+
+
+if __name__ == \"__main__\":
+    # Register signal handlers in the main thread
+    signal.signal(signal.SIGINT, signal_handler)  # Handle Ctrl+C
+    signal.signal(signal.SIGTERM, signal_handler)  # Handle termination signals
+
+    # Start the remote task
+    task_ref = wrapper.remote()
+    print(\"Task started. Waiting for signals. Press Ctrl+C to stop.\")
+
+    # Keep the main process running to listen for signals
     try:
-
-        def handle_termination_signal(signum, frame):
-            print(\"Termination signal received. Exiting immediately!\")
-            exit(0)
-
-        signal.signal(signal.SIGTERM, handle_termination_signal)
-        signal.signal(signal.SIGINT, handle_termination_signal)
-
         while True:
-            print(\"Working...\")
-            time.sleep(1)
-
-        
-    except Exception as e:
-        print(f\"Error running workflow: {e}\")
-        ray.shutdown()
-
-ray.get(main.remote())
-
+            pass
+    except KeyboardInterrupt:
+        signal_handler(signal.SIGINT, None)
 
 `
