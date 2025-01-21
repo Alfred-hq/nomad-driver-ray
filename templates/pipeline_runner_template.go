ce627e1613ea658e61e9dfad350d07852b655795
jpatidar30 <jpatidar@deqode.com>
2025-01-21 13:44:38 +0530

    feat: make actor async

--- templates/pipeline_runner_template.go
+++ templates/pipeline_runner_template.go
@@ -3,63 +3,70 @@ package templates
 // Rename dummy_template to DummyTemplate to export it
 const RayActorTemplate = `
 import ray
-import time
 import sys
 import os
 import importlib
-import psutil
+import uvloop
+import asyncio
+import signal
 
 @ray.remote(max_restarts={{.MaxActorRestarts}}, max_task_retries={{.MaxTaskRetries}})
 class {{.Actor}}:
-    def __init__(self) -> None:
-        self.finished = False
-        self.period = 180
-        self.runner_task_ref = None 
-
-    def {{.Runner}}(self):
+    async def wait_for_interrupt():
         try:
-            directory_path = os.path.dirname(\"{{.PipelineFilePath}}\")
+            await asyncio.Future()  # Await a never-completing future
+        except asyncio.CancelledError:
+            print("Interrupt received, canceling infinite loop...")
 
-            # Get the file name without the extension
-            file_name = os.path.splitext(os.path.basename(\"{{.PipelineFilePath}}\"))[0]
+    async def shutdown(loop, signal=None):
+        """Cleanup tasks tied to the service's shutdown."""
+        if signal:
+            print(f"Received exit signal {signal.name}...")
+    
+        print("Closing database connections")
+        # Add any cleanup code here (e.g., closing database connections)
+        await asyncio.sleep(1.0)
+    
+        tasks = [t for t in asyncio.all_tasks() if t is not
+                    asyncio.current_task()]
+        [task.cancel() for task in tasks]
+    
+        print(f"Cancelling {len(tasks)} outstanding tasks")
+        await asyncio.gather(*tasks, return_exceptions=True)
+        
+        await loop.shutdown_asyncgens()
+        loop.stop()
+    
 
-            sys.path.append(directory_path)
+    async def {{.Runner}}(self):
+        uvloop.install()
+        loop = asyncio.get_event_loop()
 
-            # Dynamically import the module
-            pipeline_module = importlib.import_module(file_name)
+        # Register signal handlers
+        for sig in (signal.SIGINT, signal.SIGTERM):
+            loop.add_signal_handler(
+                sig, lambda s=sig: asyncio.create_task(shutdown(loop, s))
+            )
 
-            # Execute the pipeline function
-            getattr(pipeline_module, \"{{.PipelineRunner}}\")()
-        except Exception as e:
-            print(e)
+        # Start both tasks
+        directory_path = os.path.dirname("{{.PipelineFilePath}}")
+        file_name = os.path.splitext(os.path.basename("{{.PipelineFilePath}}"))[0]
+
+        sys.path.append(directory_path)
+
+        # Dynamically import the pipeline module
+        pipeline_module = importlib.import_module(file_name)
+
+        # Execute the pipeline function directly
+        task1 = asyncio.create_task(getattr(pipeline_module, "{{.PipelineRunner}}")())
+        task2 = asyncio.create_task(wait_for_interrupt())
+        try:
+            await asyncio.gather(task1, task2)
+        except asyncio.CancelledError:
+            pass
         finally:
-            self.finished = True
             print(f\"Killing actor due to failure in runner task\")
             ray.actor.exit_actor()
-
-    def set_task_ref(self, task_ref):
-        if not self.runner_task_ref:
-            self.runner_task_ref = task_ref
-            print(\"Task started.\")
-
-    def monitor(self):
-        worker_pid = os.getpid()
-        print(f\"Monitoring PID {worker_pid}\")
-        process = psutil.Process(worker_pid)
-        while not self.finished:
-            memory_used = process.memory_info().rss
-            memory_used_mb = memory_used / (1024 ** 2)
-            print(f\"Task is using {memory_used_mb} MB\")
-            if memory_used_mb > 500:
-                print(f\"Killing actor due to memory usage above threshold\")
-                ray.cancel(self.runner_task_ref, force=True, recursive=True)
-                ray.actor.exit_actor()
-                self.finished = True
-            else:
-                print(f\"Sleeping for {self.period}s before checking memory usage again\")
-                time.sleep(self.period) 
-        print(f\"Monitor Task Completed\")
-        
 
 
 # Initialize connection to the Ray head node on the default port.
@@ -70,7 +77,6 @@ pipeline_runner = {{.Actor}}.options(name=\"{{.Actor}}\", lifetime=\"detached\",
 
 const RemoteRunnerTemplate = `
 import ray
-import time
 
 ray.init(address=\"auto\", namespace=\"{{.Namespace}}\", runtime_env={\"RAY_ENABLE_RECORD_ACTOR_TASK_LOGGING\": 1})
 
@@ -78,9 +84,7 @@ def main():
     try:
         # Get the actor
         actor = ray.get_actor(\"{{.Actor}}\")
-
-        # Trigger the actor's runner method without waiting for completion
-        runner_task_ref = actor.runner.remote()
+        actor.runner.remote()
     except Exception as e:
         print(e)
 
